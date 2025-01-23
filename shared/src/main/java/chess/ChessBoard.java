02a5e883cdcf57b1df71c7833119be36bf5ca822
Brayden Watson <bwat1@byu.edu>
2025-01-23 10:49:18 -0700

    i just commented out all of king moves calculator for noe because its throwing errors in my other tests. I tried to implement the reset board function. Now i am gonna look at position class

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -73,11 +73,81 @@ public class ChessBoard {
      */
     public void resetBoard() {
         //if you call get piece and add piece then the subtracting one is already in place
+        //white is on the bottom, black is on the top
 
-    }
+        //white
+        ChessPiece wR1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece wN1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece wB1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        ChessPiece wQ = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        ChessPiece wK = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        ChessPiece wB2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        ChessPiece wN2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece wR2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece wP1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP3 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP4 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP5 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP6 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP7 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece wP8 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 
-    public void main(String [] args) {
-        ChessBoard cb = new ChessBoard();
-        System.out.println(cb.toString());
+        //add white power pieces to the board
+        addPiece(new ChessPosition(1,1),wR1);
+        addPiece(new ChessPosition(1,2),wN1);
+        addPiece(new ChessPosition(1,3),wB1);
+        addPiece(new ChessPosition(1,4),wQ);
+        addPiece(new ChessPosition(1,5),wK);
+        addPiece(new ChessPosition(1,6),wB2);
+        addPiece(new ChessPosition(1,7),wN2);
+        addPiece(new ChessPosition(1,8),wR2);
+        //add white pawns
+        addPiece(new ChessPosition(2,1),wP1);
+        addPiece(new ChessPosition(2,2),wP2);
+        addPiece(new ChessPosition(2,3),wP3);
+        addPiece(new ChessPosition(2,4),wP4);
+        addPiece(new ChessPosition(2,5),wP5);
+        addPiece(new ChessPosition(2,6),wP6);
+        addPiece(new ChessPosition(2,7),wP7);
+        addPiece(new ChessPosition(2,8),wP8);
+
+
+        //black
+        ChessPiece bR1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece bN1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece bB1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        ChessPiece bQ = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        ChessPiece bK = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        ChessPiece bB2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        ChessPiece bN2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece bR2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece bP1 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP2 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP3 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP4 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP5 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP6 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP7 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece bP8 = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+
+        //add black power pieces to the board
+        addPiece(new ChessPosition(8,1),bR1);
+        addPiece(new ChessPosition(8,2),bN1);
+        addPiece(new ChessPosition(8,3),bB1);
+        addPiece(new ChessPosition(8,4),bQ);
+        addPiece(new ChessPosition(8,5),bK);
+        addPiece(new ChessPosition(8,6),bB2);
+        addPiece(new ChessPosition(8,7),bN2);
+        addPiece(new ChessPosition(8,8),bR2);
+        //add black pawns
+        addPiece(new ChessPosition(7,1),bP1);
+        addPiece(new ChessPosition(7,2),bP2);
+        addPiece(new ChessPosition(7,3),bP3);
+        addPiece(new ChessPosition(7,4),bP4);
+        addPiece(new ChessPosition(7,5),bP5);
+        addPiece(new ChessPosition(7,6),bP6);
+        addPiece(new ChessPosition(7,7),bP7);
+        addPiece(new ChessPosition(7,8),bP8);
     }
 }
--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,12 +1,12 @@
-package chess;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-public class KingMovesCalculator implements PieceMovesCalculator {
-    @Override
-    public Collection<ChessMove> PieceMoves(ChessBoard board, ChessPosition position) {
-        Collection<String> myCollection = new ArrayList<ChessMove>();
-
-    };
-}
+//package chess;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//
+//public class KingMovesCalculator implements PieceMovesCalculator {
+//    @Override
+//    public Collection<ChessMove> PieceMoves(ChessBoard board, ChessPosition position) {
+//        Collection<String> myCollection = new ArrayList<ChessMove>();
+//
+//    };
+//}
