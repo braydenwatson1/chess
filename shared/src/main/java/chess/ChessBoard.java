9d9b2575e9da3799afecb1f7f2e04380f4327377
Brayden Watson <bwat1@byu.edu>
2025-01-23 10:24:56 -0700

    Implemented the auto generated equals and hash in chessboard, im trying to make a tostring method and a main mathod to run it, but havent figured that out yet

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -9,6 +12,37 @@ package chess;
 public class ChessBoard {
     private ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder myboard = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                ChessPiece piece = board[i][j];
+                if (piece != null) {
+                    myboard.append(" " + piece.toString() + " ");  // Print piece
+                } else {
+                    myboard.append(" . ");  // Empty square
+                }
+            }
+            myboard.append("\n");  // Newline after each row
+        }
+        return myboard.toString();
     }
 
     /**
@@ -40,5 +74,10 @@ public class ChessBoard {
     public void resetBoard() {
         //if you call get piece and add piece then the subtracting one is already in place
 
+    }
+
+    public void main(String [] args) {
+        ChessBoard cb = new ChessBoard();
+        System.out.println(cb.toString());
     }
 }
