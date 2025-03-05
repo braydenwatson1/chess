664c6e5d168888b9da3f2024cb3674ed514788cb
Brayden Watson <bwat1@byu.edu>
2025-03-04 17:21:10 -0700

    created UserDAO interface

---
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -0,0 +1,11 @@
+package dataAccess;
+
+import model.UserData;
+
+public interface UserDAO {
+
+    void clear();
+    UserData getUser(String username);
+    void createUser(String username, String password);
+
+}
