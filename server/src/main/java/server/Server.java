97fc5ff889f903e314c177f609b3b6a81d9872bf
Brayden Watson <bwat1@byu.edu>
2025-03-05 17:44:22 -0700

    created bare bones outline for server main

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
 import spark.*;
 
 public class Server {
@@ -11,9 +13,20 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+        //init the db
+        DataAccess dbAccess;
+        dbAccess = new MemoryDataAccess();
 
+        Spark.delete("/db", HANDLERHERE::clear);
+        Spark.post("/user", HANDLERHERE::register);
+        Spark.post("/session", HANDLERHERE::login);
+        Spark.delete("/session", HANDLERHERE::logout);
+        Spark.get("/game", HANDLERHERE::listGames);
+        Spark.post("/game", HANDLERHERE::createGame);
+        Spark.put("/game", HANDLERHERE::joinGame);
 
-
+        Spark.exceptions...{FILL IN LATER}
+        
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
