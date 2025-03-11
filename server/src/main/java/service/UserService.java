bb53c43de4c2f39f7406d787dbd7c6d2319ed1a8
Brayden Watson <bwat1@byu.edu>
2025-03-10 21:07:44 -0600

    userservice complete

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -17,28 +17,30 @@ public class UserService {
     }
 
         public RegisterResult register(RegisterRequest regReq) throws BadRequestException, DataAccessException {
-
             //if request is bad, error
             if (regReq == null || regReq.username() == null || regReq.password() == null || regReq.email() == null) {
                 throw new BadRequestException("Error: Username, Password, and email cannot be null. See UserService class: register() function");
             }
 
+            String myUsername = regReq.username();
+            String myPassword = regReq.password();
+            String myEmail = regReq.email();
+
             //create UserData object to insert into db
-            UserData newUser = new UserData(regReq.username(),regReq.password(),regReq.email());
+            UserData newUser = new UserData(myUsername, myPassword, myEmail);
 
             //call createUser function, which inherently checks if
             // user already exists, and adds it to the db if it doesn't
             dbAccess.getUserDAO().createUser(newUser);
 
             //create authToken
-            String authTokenNum = UUID.randomUUID().toString();
-            String associatedUsername = regReq.username();
-            AuthData newAuthToken = new AuthData(associatedUsername, authTokenNum);
+            String authToken = UUID.randomUUID().toString();
+            AuthData newAuthData = new AuthData(myUsername, authToken);
 
             //add authToken to db
-            dbAccess.getAuthDAO().addAuth(newAuthToken);
+            dbAccess.getAuthDAO().addAuth(newAuthData);
 
-            RegisterResult FinalResult = new RegisterResult(associatedUsername, authTokenNum);
+            RegisterResult FinalResult = new RegisterResult(myUsername, authToken);
 
             //return the authToken & username
             return FinalResult;
@@ -50,29 +52,30 @@ public class UserService {
                 throw new BadRequestException("Error: Username, Password cannot be null. See UserService class: register() function");
             }
 
+            String myUsername = logReq.username();
+            String myPassword = logReq.password();
+
             //authorization
-            String userUsername = logReq.username();
             UserData user;
             try {
-                user = dbAccess.getUserDAO().getUser(userUsername);
+                user = dbAccess.getUserDAO().getUser(myUsername);
             } catch (DataAccessException e) {
-                throw new BadRequestException("User not found: " + userUsername);
+                throw new BadRequestException("User not found: " + myUsername);
             }
 
             // is password correct?
-            if (!Objects.equals(user.password(), logReq.password())) {
+            if (!Objects.equals(user.password(), myPassword)) {
                 throw new BadRequestException("password is incorrect");
             }
 
             //create authToken
             String authTokenNum = UUID.randomUUID().toString();
-            String associatedUsername = logReq.username();
-            AuthData newAuthToken = new AuthData(associatedUsername, authTokenNum);
+            AuthData newAuthToken = new AuthData(myUsername, authTokenNum);
 
             //add authToken to db
             dbAccess.getAuthDAO().addAuth(newAuthToken);
 
-            LoginResult FinalResult = new LoginResult(associatedUsername, authTokenNum);
+            LoginResult FinalResult = new LoginResult(myUsername, authTokenNum);
 
             //return the authToken & username
             return FinalResult;
@@ -96,7 +99,7 @@ public class UserService {
             //this function deletes the whole authData object, not just the
             // authtoken string from the object it finds it in
             dbAccess.getAuthDAO().deleteAuth(auth.authToken());
-
         }
+        
 }
 
