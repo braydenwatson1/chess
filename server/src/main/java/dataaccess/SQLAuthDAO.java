06187da0633fe82fdf1ec72d96f0a676de77ee2f
Brayden Watson <bwat1@byu.edu>
2025-03-19 17:09:24 -0600

    add auth, get auth, delete auth functions for sqlAuthDao

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,10 +1,9 @@
 package dataaccess;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import Model.AuthData;
+
+import java.sql.*;
 
 public class SQLAuthDAO implements AuthDAO {
 
@@ -36,6 +35,51 @@ public class SQLAuthDAO implements AuthDAO {
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException("Error clearing auth table: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void addAuth(AuthData newAuthData) throws DataAccessException {
+        String sql = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, newAuthData.username());
+            stmt.setString(2, newAuthData.authToken());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error adding auth: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public AuthData getAuth(String myAuthToken) throws DataAccessException {
+        String sql = "SELECT username, authToken FROM auth WHERE authToken = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, myAuthToken);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(rs.getString("username"), rs.getString("authToken"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error retrieving auth: " + e.getMessage());
+        }
+        throw new DataAccessException("Auth Token not found: " + myAuthToken);
+    }
+
+    @Override
+    public void deleteAuth(String myAuthToken) throws DataAccessException {
+        String sql = "DELETE FROM auth WHERE authToken = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, myAuthToken);
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DataAccessException("Auth Token does not exist, unable to delete: " + myAuthToken);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error deleting auth: " + e.getMessage());
         }
     }
 
