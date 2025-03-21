8f57e9149c674f39835314cbf4d0fb44cdecdfd4
Brayden Watson <bwat1@byu.edu>
2025-03-20 21:19:20 -0600

    phase 4 done? idk

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,7 +9,7 @@ import spark.*;
 
 public class Server {
 
-    public int run(int desiredPort) {
+    public int run(int desiredPort) throws DataAccessException {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
