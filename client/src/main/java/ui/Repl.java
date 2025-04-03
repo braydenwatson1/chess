de859ddfab826011f18277ff41596b588340cd35
Brayden Watson <bwat1@byu.edu>
2025-04-03 16:35:05 -0600

    outer repl complete?(i think)

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -5,6 +5,7 @@ import Model.LoginResult;
 import Model.RegisterRequest;
 import Model.RegisterResult;
 import client.ServerFacade;
+import com.google.gson.Gson;
 
 import java.util.Scanner;
 
@@ -33,30 +34,46 @@ public class Repl {
             }
             else if (results[0].equals("login")) {
                 if (results.length != 3) {
+                    System.out.println(SET_TEXT_COLOR_RED);
                     System.out.println("Please follow this format: ");
                     System.out.println("to login, enter: 'login USERNAME PASSWORD'");
-
+                    System.out.println(RESET_TEXT_COLOR);
                 }
                 try {
                     LoginResult res = server.login(new LoginRequest(results[1], results[2]));
-//                    innerRepl.run(server);
-                    System.out.println("logging in...");
+
+                    if (res.authToken() != null) {  // Assuming LoginResult has getAuthToken()
+                        System.out.println(SET_TEXT_COLOR_GREEN + "Login successful!" + RESET_TEXT_COLOR);
+                        System.out.println("DEBUG: Server response -> " + new Gson().toJson(res));
+
+                        // innerRepl.run(server); // Uncomment when InnerRepl is ready
+                    } else {
+                        System.out.println(SET_TEXT_COLOR_RED + "Login failed." + RESET_TEXT_COLOR);
+                    }
+
+                    System.out.println(SET_TEXT_COLOR_YELLOW + "You have been logged out." + RESET_TEXT_COLOR);
                 } catch (Exception e) {  // Catch any exception
-                    System.out.println("Username and password invalid " + e.getMessage());  // Print the error message
+                    System.out.println(SET_TEXT_COLOR_RED);
+                    System.out.println("Username and password invalid");  // Print the error message
+                    System.out.println(RESET_TEXT_COLOR);
                 }
             }
             else if (results[0].equals("register")) {
                 if (results.length != 4) {
+                    System.out.println(SET_TEXT_COLOR_RED);
                     System.out.println("Please follow this format: ");
                     System.out.println("to register, enter: 'register USERNAME PASSWORD EMAIL'");
-
+                    System.out.println(RESET_TEXT_COLOR);
                 }
                 try {
+                    System.out.println("Registering...");
                     RegisterResult res = server.register(new RegisterRequest(results[1], results[2], results[3]));
-//                    innerRepl.run(server);
-                    System.out.println("registering...");
-                } catch (Exception e) {  // Catch any exception
-                    System.out.println("Registration failed: " + e.getMessage());  // Print the error message
+                    System.out.println("Logging you in...");
+                    LoginResult Lres = server.login(new LoginRequest(results[1], results[2]));
+                    //innerRepl.run(server);
+                    System.out.println(SET_TEXT_COLOR_YELLOW + "You have been logged out." + RESET_TEXT_COLOR);
+                } catch (Exception e) {
+                    System.out.println("Registration failed:");
                 }
             }
             else if (results[0].equals("quit")) {
